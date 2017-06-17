a53fdc0e7717b3609471c20fcf3e8fb90b2c09b7
Paul WOISARD <paulwoisard@gmail.com>
2017-06-17 15:47:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ fi
 if [ ! -d "$HOME/.icons/" ]; then
   mkdir $HOME/.icons/
 fi
-cp MiseAJourEPG.sh $HOME/Documents/MiseAJourEPG/
+cp MiseAJourEPG.sh $HOME/.local/bin/
 cp tvguide-logo-epg.png $HOME/.icons
 cp MiseAJourEPG.desktop $HOME/.local/share/applications/
-chmod +x $HOME/Documents/MiseAJourEPG/MiseAJourEPG.sh
+chmod +x $HOME/.local/bin/MiseAJourEPG.sh
 chmod +x $HOME/.local/share/applications/MiseAJourEPG.desktop
 chmod +x ./uninstall.sh
