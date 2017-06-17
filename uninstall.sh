3e6d20a73cb574e0b1b8c76fc14816d7289d2db0
Paul WOISARD <paulwoisard@gmail.com>
2017-06-17 15:56:51 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -rf $HOME/Documents/MiseAJourEPG/
 rm -rf $HOME/.local/share/applications/MiseAJourEPG.desktop
+rm -rf $HOME/.local/bin/MiseAJourEPG.sh
 rm -rf $HOME/.icons/tvguide-logo-epg.png
